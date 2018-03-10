285135a75a71a629bea9ce893f75ca79a916af42
jiang-12196 <jiang12196@126.com>
2018-03-10 10:48:29 +0800

    add some config

---
+++ test/baidu_test.js
@@ -0,0 +1,14 @@
+Feature('Baidu');
+
+Scenario('test something', I => {
+  I.amOnPage('http://www.baidu.com');
+  I.see('百度');
+  I.fillField('#kw', '千米网');
+  I.click('//*[@id="su"]');
+
+  I.waitForText('专注于新分销和新零售的电商化运营', 5);
+  I.click('专注于新分销和新零售的电商化运营');
+  I.wait(10);
+  // I.click('About');
+  // I.see('About Node.js')
+});
