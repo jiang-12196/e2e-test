99fa0d6af89de2782b8f0a5efda9cd6fb25d4d5a
jiang-12196 <jiang12196@126.com>
2018-03-17 15:53:13 +0800

    add login test case

---
+++ test/login_test.js
@@ -0,0 +1,46 @@
+const { name, password } = require('../config');
+const fields = {
+  userName: '#nickName0',
+  pwd: '#logPsw',
+  button: '#loginForCompany > button'
+};
+
+Feature('千米网登录系统');
+
+Scenario('登录账号', (I, loginPage) => {
+  loginPage.beforeLogin();
+});
+
+Scenario('登录表单校验', I => {
+  I.click(fields.button);
+  I.see('请输入绑定手机号');
+  I.fillField(fields.userName, '187');
+  I.click(fields.button);
+  I.see('手机号格式不正确');
+  I.fillField(fields.userName, name);
+  I.click(fields.button);
+  I.see('请输入密码');
+  I.fillField(fields.pwd, '123');
+  I.click(fields.button);
+  I.waitForText('手机号或密码不正确', 3);
+  I.click(
+    '#loginForCompany > div > div > span > input'
+  );
+  I.seeCheckboxIsChecked(
+    '#loginForCompany > div > div > span > input'
+  );
+  I.click(
+    '#loginForCompany > div > div > span > input'
+  );
+  I.dontSeeCheckboxIsChecked(
+    '#loginForCompany > div > div > span > input'
+  );
+});
+
+Scenario('登录账号', (I, loginPage) => {
+  loginPage.loginAccount(name, password);
+});
+
+Scenario('退出账号', (I, loginPage) => {
+  loginPage.logout();
+});
