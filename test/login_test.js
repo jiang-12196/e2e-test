149fc04675b3da86e39997c3d21912707517d5f5
jiang-12196 <jiang12196@126.com>
2018-03-20 18:19:43 +0800

    add login test

--- test/login_test.js
+++ test/login_test.js
@@ -2,7 +2,7 @@ const { name, password } = require('../config');
 const fields = {
   userName: '#nickName0',
   pwd: '#logPsw',
-  button: '#loginForCompany > button'
+  submitButton: '#loginForCompany > submitButton'
 };
 
 Feature('千米网登录系统');
@@ -12,29 +12,21 @@ Scenario('登录账号', (I, loginPage) => {
 });
 
 Scenario('登录表单校验', I => {
-  I.click(fields.button);
+  I.click(fields.submitButton);
   I.see('请输入绑定手机号');
   I.fillField(fields.userName, '187');
-  I.click(fields.button);
+  I.click(fields.submitButton);
   I.see('手机号格式不正确');
   I.fillField(fields.userName, name);
-  I.click(fields.button);
+  I.click(fields.submitButton);
   I.see('请输入密码');
   I.fillField(fields.pwd, '123');
-  I.click(fields.button);
+  I.click(fields.submitButton);
   I.waitForText('手机号或密码不正确', 3);
-  I.click(
-    '#loginForCompany > div > div > span > input'
-  );
-  I.seeCheckboxIsChecked(
-    '#loginForCompany > div > div > span > input'
-  );
-  I.click(
-    '#loginForCompany > div > div > span > input'
-  );
-  I.dontSeeCheckboxIsChecked(
-    '#loginForCompany > div > div > span > input'
-  );
+  I.click('#loginForCompany > div > div > span > input');
+  I.seeCheckboxIsChecked('#loginForCompany > div > div > span > input');
+  I.click('#loginForCompany > div > div > span > input');
+  I.dontSeeCheckboxIsChecked('#loginForCompany > div > div > span > input');
 });
 
 Scenario('登录账号', (I, loginPage) => {
